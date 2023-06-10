fa23e74b504c73606c937791bafdd5e2225f6559
Caio Silva <caiossantosp5560@gmail.com>
2023-06-09 22:12:35 -0300

    feat:adicionar tabela de usuários

--- src/adminjs/resource/index.ts
+++ src/adminjs/resource/index.ts
@@ -3,6 +3,8 @@ import { Category, Course, Episode } from "../../models"
 import { categoryResourceOptions } from "./category"
 import { courseResourceOptions, courseResourceFeatures } from "./course"
 import { episodeResourceOptions, episodeResourceFeatures } from "./episode"
+import { userResourceOptions } from "./user"
+import { User } from "../../models/User"
 
 
 export const adminJsResources: ResourceWithOptions[] = [
@@ -20,4 +22,8 @@ export const adminJsResources: ResourceWithOptions[] = [
     options: episodeResourceOptions,
     features: episodeResourceFeatures
   },
+  {
+    resource: User,
+    options: userResourceOptions
+  }
 ]
