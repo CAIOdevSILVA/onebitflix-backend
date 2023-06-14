255e7e6006088ce787ed6e9823511a41bc201c35
Caio Silva <caiossantosp5560@gmail.com>
2023-06-14 15:43:40 -0300

    feat: adicionar streaming de video

--- src/controllers/episodesController.ts
+++ src/controllers/episodesController.ts
@@ -1,13 +1,20 @@
-import { Request, Response } from 'express' 
+import { Request, Response } from "express";
+import { episodeService } from "../services/episodeServices";
+
 
 export const episodesController = {
   //GET /episodes/stream?videoUrl=
   stream: async(req: Request, res: Response) => {
     const { videoUrl } = req.query;
     try {
-      
+      if(typeof videoUrl !== "string") throw new Error('videoUrl param must be type of string');
+      const range = req.headers.range  //determina a parte do video que sera carregada
+      episodeService.streamEpisodeToResponse(res, videoUrl, range);
+
     } catch (error) {
-      
+      if(error instanceof Error){
+        return res.status(400).json({ message: error.message });
+      }
     }
-  }
+  },
 }
