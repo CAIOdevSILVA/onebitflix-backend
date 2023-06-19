ec510635e970a14df07b67f192d338956d8472cf
Caio Silva <caiossantosp5560@gmail.com>
2023-06-19 11:06:55 -0300

    feat: adicionar funcionalidade de favoritos

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,17 +1,28 @@
 import { Category } from "./Category";
 import { Course } from "./Course";
 import { Episode } from "./episode";
+import { Favorite } from "./Favorite";
 import { User } from "./User";
 
 Category.hasMany(Course, { as: 'courses' });
-Course.belongsTo(Category); 
 
+Course.belongsTo(Category); 
 Course.hasMany(Episode, { as: 'episodes' });//na association o sequelize por padrão fara: 'Episodes"
+Course.belongsToMany(User, { through: Favorite });
+Course.hasMany(Favorite, { as: "FavoritesUsers", foreignKey: "course_id" });
+
 Episode.belongsTo(Course);
+
+Favorite.belongsTo(Course);
+Favorite.belongsTo(User);
+
+User.belongsToMany(Course, { through: Favorite });
+User.hasMany(Favorite, { as: "FavoritesCourses", foreignKey: "user_id" });
 
 export {
   Category,
   Course,
   Episode,
-  User
+  User,
+  Favorite
 };
--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,7 @@ import { coursesController } from "./controllers/coursesController";
 import { episodesController } from "./controllers/episodesController";
 import { authController } from "./controllers/authController";
 import { ensureAuth, ensureAuthViaQuery } from "./middlewares/auth";
+import { favoritesController } from "./controllers/favoritesController";
 
 const router = express.Router();
 
@@ -17,6 +18,8 @@ router.get('/courses/featured',  ensureAuth, coursesController.featured);
 router.get('/courses/newest', coursesController.newest);
 router.get('/courses/search', ensureAuth, coursesController.search);
 router.get('/courses/:id', ensureAuth, coursesController.show);
+
+router.post('/favorites', ensureAuth, favoritesController.save);
 
 router.get('/episodes/stream', ensureAuthViaQuery,episodesController.stream);
 
