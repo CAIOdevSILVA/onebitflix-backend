eabdc7019b30cd987652d4337a35b0a3e496fccb
Caio Silva <caiossantosp5560@gmail.com>
2023-06-22 19:23:37 -0300

    feat: adicionando cors

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import express from "express";
+import cors from "cors"
 import { sequelize } from "./database";
 import { adminJs, adminJsRouter } from "./adminjs";
 import { router } from "./routes";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 app.use(adminJs.options.rootPath, adminJsRouter);
